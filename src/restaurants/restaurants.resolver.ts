165e550e384108a1ea89abfe9ad175d79c8dba14
Chang Yeob Jin <jincy713@gmail.com>
2020-12-27 11:56:22 +0900

    feat :[GraphQL] 쿼리에 argument 추가.

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,11 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver(() => Restaurant)
 export class RestaurantsResolver{
 
-  @Query(() => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query(() => [Restaurant])
+  restaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
   }
 }
